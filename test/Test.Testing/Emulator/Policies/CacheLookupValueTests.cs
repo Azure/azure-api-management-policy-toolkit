7d31db2337158fdafda7da6edd4cf9859fe0b72b
Rafal Mielowski <rmielowski@microsoft.com>
2025-01-10 11:42:38 +0100

    Add tests

--- test/Test.Testing/Emulator/Policies/CacheLookupValueTests.cs
+++ test/Test.Testing/Emulator/Policies/CacheLookupValueTests.cs
@@ -10,18 +10,54 @@ namespace Test.Emulator.Emulator.Policies;
 [TestClass]
 public class CacheLookupValueTests
 {
-    class SimpleCacheLookupValue : IDocument
+    class SimpleCacheLookupValuePreferExternal : IDocument
     {
         public void Inbound(IInboundContext context)
         {
-            context.CacheLookupValue(new CacheLookupValueConfig() { Key = "key", VariableName = "variable" });
+            context.CacheLookupValue(new CacheLookupValueConfig()
+            {
+                Key = "key", VariableName = "variable", CachingType = "prefer-external"
+            });
+        }
+    }
+
+    class SimpleCacheLookupValueFromInternal : IDocument
+    {
+        public void Inbound(IInboundContext context)
+        {
+            context.CacheLookupValue(new CacheLookupValueConfig()
+            {
+                Key = "key", VariableName = "variable", CachingType = "internal"
+            });
+        }
+    }
+
+    class SimpleCacheLookupValueFromExternal : IDocument
+    {
+        public void Inbound(IInboundContext context)
+        {
+            context.CacheLookupValue(new CacheLookupValueConfig()
+            {
+                Key = "key", VariableName = "variable", CachingType = "external"
+            });
+        }
+    }
+
+    class SimpleCacheLookupValueWithDefaultValue : IDocument
+    {
+        public void Inbound(IInboundContext context)
+        {
+            context.CacheLookupValue(new CacheLookupValueConfig()
+            {
+                Key = "key", VariableName = "variable", DefaultValue = "test-value"
+            });
         }
     }
 
     [TestMethod]
     public void CacheLookupValue_Callback()
     {
-        var test = new SimpleCacheLookupValue().AsTestDocument();
+        var test = new SimpleCacheLookupValuePreferExternal().AsTestDocument();
         var executedCallback = false;
         test.SetupInbound().CacheLookupValue().WithCallback((_, _) =>
         {
@@ -34,9 +70,9 @@ public class CacheLookupValueTests
     }
 
     [TestMethod]
-    public void CacheLookupValue_WithValue()
+    public void CacheLookupValue_WithValueCallback()
     {
-        var test = new SimpleCacheLookupValue().AsTestDocument();
+        var test = new SimpleCacheLookupValuePreferExternal().AsTestDocument();
         test.SetupInbound().CacheLookupValue().WithValue("test");
 
         test.RunInbound();
@@ -45,9 +81,9 @@ public class CacheLookupValueTests
     }
 
     [TestMethod]
-    public void CacheLookupValue_SetupCacheStore_WithInternalValue()
+    public void CacheLookupValue_PreferExternal_SetupCacheStore_WithInternalValue()
     {
-        var test = new SimpleCacheLookupValue().AsTestDocument();
+        var test = new SimpleCacheLookupValuePreferExternal().AsTestDocument();
         test.SetupCacheStore().WithInternalCacheValue("key", "test");
 
         test.RunInbound();
@@ -55,15 +91,67 @@ public class CacheLookupValueTests
         test.Context.Variables.Should().ContainKey("variable").WhoseValue.Should().Be("test");
     }
 
-
     [TestMethod]
-    public void CacheLookupValue_SetupCacheStore_WithExternalCacheSetup()
+    public void CacheLookupValue_PreferExternal_SetupCacheStore_WithExternalCacheSetup()
     {
-        var test = new SimpleCacheLookupValue().AsTestDocument();
+        var test = new SimpleCacheLookupValuePreferExternal().AsTestDocument();
         test.SetupCacheStore().WithExternalCacheSetup().WithExternalCacheValue("key", "test");
 
         test.RunInbound();
 
         test.Context.Variables.Should().ContainKey("variable").WhoseValue.Should().Be("test");
+    }
+
+    [TestMethod]
+    public void CacheLookupValue_WithDefaultValue()
+    {
+        var test = new SimpleCacheLookupValueWithDefaultValue().AsTestDocument();
+
+        test.RunInbound();
+
+        test.Context.Variables.Should().ContainKey("variable").WhoseValue.Should().Be("test-value");
+    }
+
+    [TestMethod]
+    public void CacheLookupValue_InternalCache_WillNotFindValueFromExternalCache()
+    {
+        var test = new SimpleCacheLookupValueFromInternal().AsTestDocument();
+        test.SetupCacheStore().WithExternalCacheSetup().WithExternalCacheValue("key", "test");
+
+        test.RunInbound();
+
+        test.Context.Variables.Should().NotContainKey("variable");
+    }
+
+    [TestMethod]
+    public void CacheLookupValue_Internal_WillNotFindValueInInternalCache()
+    {
+        var test = new SimpleCacheLookupValueFromInternal().AsTestDocument();
+
+        test.RunInbound();
+
+        test.Context.Variables.Should().NotContainKey("variable");
+    }
+
+    [TestMethod]
+    public void CacheLookupValue_ExternalCache_WillNotFindValueInInternalCache()
+    {
+        var test = new SimpleCacheLookupValueFromExternal().AsTestDocument();
+        test.SetupCacheStore().WithInternalCacheValue("key", "test");
+
+        test.RunInbound();
+
+        test.Context.Variables.Should().NotContainKey("variable");
+    }
+
+    [TestMethod]
+    public void CacheLookupValue_ExternalCache_WillNotFindValue_WhenExternalCacheNotSetup()
+    {
+        var test = new SimpleCacheLookupValueFromExternal().AsTestDocument();
+        test.SetupCacheStore().WithExternalCacheValue("key", "test");
+
+        test.RunInbound();
+
+        test.Context.Variables.Should().NotContainKey("variable");
     }
 }
