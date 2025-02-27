5d41326170f7028ec0f2dcb4c8afb69e600ced91
Rafal Mielowski <rmielowski@microsoft.com>
2025-02-27 15:45:44 +0100

    Add wait policy to compiler

---
+++ src/Core/Compiling/Policy/WaitCompiler.cs
@@ -0,0 +1,68 @@
+using System.Xml.Linq;
+
+using Azure.ApiManagement.PolicyToolkit.Authoring;
+using Azure.ApiManagement.PolicyToolkit.Compiling.Diagnostics;
+using Azure.ApiManagement.PolicyToolkit.Compiling.Syntax;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Azure.ApiManagement.PolicyToolkit.Compiling.Policy;
+
+public class WaitCompiler : IMethodPolicyHandler
+{
+    private readonly Lazy<BlockCompiler> _blockCompiler;
+
+    public WaitCompiler(Lazy<BlockCompiler> blockCompiler)
+    {
+        _blockCompiler = blockCompiler;
+    }
+
+    public string MethodName { get; } = nameof(IInboundContext.Wait);
+
+    public void Handle(ICompilationContext context, InvocationExpressionSyntax node)
+    {
+        if (node.ArgumentList.Arguments.Count is > 2 or < 1)
+        {
+            context.Report(Diagnostic.Create(
+                CompilationErrors.ArgumentCountMissMatchForPolicy,
+                node.ArgumentList.GetLocation(),
+                "wait"));
+            return;
+        }
+
+        ExpressionSyntax childPoliciesLambdaExpression = node.ArgumentList.Arguments[0].Expression;
+        if (childPoliciesLambdaExpression is not LambdaExpressionSyntax lambda)
+        {
+            context.Report(Diagnostic.Create(
+                CompilationErrors.ValueShouldBe,
+                childPoliciesLambdaExpression.GetLocation(),
+                "wait",
+                nameof(LambdaExpressionSyntax)));
+            return;
+        }
+
+        if (lambda.Block is null)
+        {
+            context.Report(Diagnostic.Create(
+                CompilationErrors.NotSupportedStatement,
+                lambda.GetLocation(),
+                childPoliciesLambdaExpression.GetType().FullName
+            ));
+            return;
+        }
+
+        XElement element = new("wait");
+
+        if (node.ArgumentList.Arguments.Count == 2)
+        {
+            string value = node.ArgumentList.Arguments[1].Expression.ProcessParameter(context);
+            element.Add(new XAttribute("for", value));
+        }
+
+        SubCompilationContext subContext = new(context, element);
+        _blockCompiler.Value.Compile(subContext, lambda.Block);
+
+        context.AddPolicy(element);
+    }
+}
