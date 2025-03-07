acb3056ea92f63ed1382332e2715f56ef739a4a6
Rafal Mielowski <rmielowski@microsoft.com>
2025-03-07 12:03:50 +0100

    Add log to eventhub policy to compiler

---
+++ src/Core/Compiling/Policy/LogToEventHubCompiler.cs
@@ -0,0 +1,67 @@
+using System.Xml.Linq;
+
+using Azure.ApiManagement.PolicyToolkit.Authoring;
+using Azure.ApiManagement.PolicyToolkit.Compiling.Diagnostics;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Azure.ApiManagement.PolicyToolkit.Compiling.Policy;
+
+public class LogToEventHubCompiler : IMethodPolicyHandler
+{
+    public string MethodName => nameof(IInboundContext.LogToEventHub);
+
+    public void Handle(ICompilationContext context, InvocationExpressionSyntax node)
+    {
+        if (!node.TryExtractingConfigParameter<LogToEventHubConfig>(context, "log-to-eventhub",
+                out IReadOnlyDictionary<string, InitializerValue>? values))
+        {
+            return;
+        }
+
+        XElement element = new("log-to-eventhub");
+        if (!element.AddAttribute(values, nameof(LogToEventHubConfig.LoggerId), "logger-id"))
+        {
+            context.Report(Diagnostic.Create(
+                CompilationErrors.RequiredParameterNotDefined,
+                node.GetLocation(),
+                "log-to-eventhub",
+                nameof(LogToEventHubConfig.LoggerId)
+            ));
+            return;
+        }
+
+        bool addedPartitionKey =
+            element.AddAttribute(values, nameof(LogToEventHubConfig.PartitionKey), "partition-key");
+        bool addedPartitionId = element.AddAttribute(values, nameof(LogToEventHubConfig.PartitionId), "partition-id");
+
+        if (addedPartitionKey && addedPartitionId)
+        {
+            context.Report(Diagnostic.Create(
+                CompilationErrors.OnlyOneOfTwoShouldBeDefined,
+                node.GetLocation(),
+                "log-to-eventhub",
+                nameof(LogToEventHubConfig.PartitionKey),
+                nameof(LogToEventHubConfig.PartitionId)
+            ));
+            return;
+        }
+
+        if (!values.TryGetValue(nameof(LogToEventHubConfig.Value), out InitializerValue? initializerValue) ||
+            initializerValue.Value is null)
+        {
+            context.Report(Diagnostic.Create(
+                CompilationErrors.RequiredParameterNotDefined,
+                node.GetLocation(),
+                "log-to-eventhub",
+                nameof(LogToEventHubConfig.Value)
+            ));
+            return;
+        }
+
+        element.Add(initializerValue.Value);
+
+        context.AddPolicy(element);
+    }
+}
