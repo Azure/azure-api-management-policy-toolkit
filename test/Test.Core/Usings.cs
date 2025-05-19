471da2609ba2357d9f24255162ba48d9bc856bc0
Rafal Mielowski <rmielowski@microsoft.com>
2025-05-19 14:13:05 +0200

    Fix imports ordering

--- test/Test.Core/Usings.cs
+++ test/Test.Core/Usings.cs
@@ -1,9 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-global using Microsoft.Azure.ApiManagement.PolicyToolkit.Assertions;
-global using Microsoft.Azure.ApiManagement.PolicyToolkit.Tests.Extensions;
-
 global using FluentAssertions;
 
+global using Microsoft.Azure.ApiManagement.PolicyToolkit.Assertions;
+global using Microsoft.Azure.ApiManagement.PolicyToolkit.Tests.Extensions;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
